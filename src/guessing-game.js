18860b676e53a6d0469f1d6a1d216ce9a7fd772a
iMaxZorky <mkoprav@gmail.com>
2023-02-19 16:14:17 +0600

    update: all done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+      this.min = null;
+      this.max = null;
+      this.solution = null;
+    }
+  
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
-
+  
     guess() {
-
+      this.solution = Math.round((this.max + this.min) / 2);
+      return this.solution;
     }
-
+  
     lower() {
-
+      this.max = this.solution;
     }
-
+  
     greater() {
-
+      this.min = this.solution;
     }
-}
+  }
 
 module.exports = GuessingGame;
